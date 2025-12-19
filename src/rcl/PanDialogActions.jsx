0148abe8c492ef9f31643ea3cc3be93ffc81bc4b
lmeline <loise13@hotmail.fr>
2025-12-19 13:56:01 +0100

    switch panDialogButton in panDialogActions

--- src/rcl/PanDialogActions.jsx
+++ src/rcl/PanDialogActions.jsx
@@ -3,7 +3,7 @@ import PanDialogButton from "./PanDialogInternals/PanDialogButton";
 
 export default function PanDialogActions ({actionFn, actionLabel, closeFn, closeLabel, isDisabled}) {
     return <DialogActions>
-        <PanDialogButton actionFn={() => {closeFn(); actionFn();}} isDisabled={isDisabled} label={actionLabel}/>
         <PanDialogButton actionFn={closeFn} isDisabled={false} label={closeLabel}/>
+        <PanDialogButton actionFn={() => {closeFn(); actionFn();}} isDisabled={isDisabled} label={actionLabel}/>
         </DialogActions>
 }
