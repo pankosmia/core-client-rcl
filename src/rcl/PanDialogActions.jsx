2e4f5e56e7616d1eafef6efe80e711ddcc414bad
lmeline <loise13@hotmail.fr>
2026-01-06 15:01:17 +0100

    delete closefn from button

--- src/rcl/PanDialogActions.jsx
+++ src/rcl/PanDialogActions.jsx
@@ -4,6 +4,6 @@ import PanDialogButton from "./PanDialogInternals/PanDialogButton";
 export default function PanDialogActions ({actionFn, actionLabel, closeFn, closeLabel, isDisabled}) {
     return <DialogActions>
         <PanDialogButton actionFn={closeFn} isDisabled={false} label={closeLabel}/>
-        <PanDialogButton actionFn={() => {closeFn(); actionFn();}} isDisabled={isDisabled} label={actionLabel}/>
+        <PanDialogButton actionFn={() => {actionFn();}} isDisabled={isDisabled} label={actionLabel}/>
         </DialogActions>
 }
