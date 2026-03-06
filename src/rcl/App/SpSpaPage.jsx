006a1c1e75d1ebfef273b8195ab91e8b3aeabf42
lmeline <loise13@hotmail.fr>
2026-03-06 13:29:17 +0100

    i18n remote repo

--- src/rcl/App/SpSpaPage.jsx
+++ src/rcl/App/SpSpaPage.jsx
@@ -1,49 +1,58 @@
-import React, {useContext} from "react";
-import {Box, Grid2} from "@mui/material";
+import React, { useContext } from "react";
+import { Box, Grid2 } from "@mui/material";
 import BlockOutlined from "@mui/icons-material/BlockOutlined";
 import Header from "../Header/Header";
-import {doI18n } from "pithekos-lib";
+import { doI18n } from "pithekos-lib";
 import netContext from "../contexts/netContext";
 import i18nContext from "../contexts/i18nContext";
 
-function SpSpaPage({titleKey, widget, margin = 0, children, requireNet = false, currentId}) {
-    const {enableNet} = useContext(netContext);
-    const i18n = useContext(i18nContext);
+function SpSpaPage({ titleKey, widget, margin = 0, children, requireNet = false, currentId }) {
+    const { enableNet } = useContext(netContext);
+    const { i18nRef } = useContext(i18nContext);
+
     if (requireNet && !enableNet) {
         return (<Box sx={{
-                m: margin,
-                height: "100%",
-                overflowX: "hidden",
-                overflowY: "auto",
-            }}>
-                <Grid2 container spacing={0} direction="column" alignItems="center" justifyContent="center"
-                       sx={{minHeight: '100vh'}}>
-                    <Grid2 item>
-                        <Box>
-                            <BlockOutlined fontSize="large" color="warning"/>
-                            <p>{doI18n("components:framework:no_entry_if_offline", i18n)}</p>
-                        </Box>
-                    </Grid2>
+            m: 0,
+            height: "100%",
+            overflowX: "hidden",
+            overflowY: "auto",
+        }}>
+            <Grid2
+                container
+                alignItems="center"
+                justifyContent="center"
+                sx={{ minHeight: "100vh" }}
+            >
+                <Grid2
+                    item
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    rowSpacing={0.5}
+                >
+                    <BlockOutlined fontSize="large" color="warning" />
+                    <p>{doI18n("components:framework:no_entry_if_offline", i18nRef.current)}</p>
                 </Grid2>
-            </Box>
+            </Grid2>
+        </Box>
         );
     }
     return (<>
-            <Box>
-                <Header
-                    titleKey={titleKey || null}
-                    widget={widget || null}
-                    currentId={currentId}
-                />
-            </Box>
-            <Box sx={{
-                m: margin,
-                overflowX: "hidden",
-                overflowY: "auto",
-            }}>
-                {children}
-            </Box>
-        </>
+        <Box>
+            <Header
+                titleKey={titleKey || null}
+                widget={widget || null}
+                currentId={currentId}
+            />
+        </Box>
+        <Box sx={{
+            m: margin,
+            overflowX: "hidden",
+            overflowY: "auto",
+        }}>
+            {children}
+        </Box>
+    </>
     );
 }
 
