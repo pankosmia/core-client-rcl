98e24ca28ee8d4d5ede6f99a22a6c0fc8f4264a6
lmeline <loise13@hotmail.fr>
2026-05-20 09:49:28 +0200

    commencement nouveau bouton hors ligne

--- src/rcl/InternetSwitch.jsx
+++ src/rcl/InternetSwitch.jsx
@@ -1,40 +1,68 @@
-import React, { useState } from "react";
-import { Box, Chip } from "@mui/material";
-import AirplanemodeInactiveOutlinedIcon from "@mui/icons-material/AirplanemodeInactiveOutlined";
-import AirplanemodeActiveOutlinedIcon from "@mui/icons-material/AirplanemodeActiveOutlined";
-import { doI18n, postEmptyJson } from "pithekos-lib";
+import React, { useContext, useEffect, useState } from "react";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import { doI18n, getJson, postEmptyJson } from "pithekos-lib";
 import InternetWarningDialog from "./InternetWarningDialog";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import I18nContext from "./contexts/i18nContext";
+export default function InternetSwitch({ netEnabled, debug = false }) {
+  const { i18nRef } = useContext(I18nContext);
 
-export default function InternetSwitch({ i18n, netEnabled, debug = false }) {
   const [internetDialogOpen, setInternetDialogOpen] = useState(false);
+  const [nameProduct, setNameProduct] = useState("");
 
+  const [alignment, setAlignment] = React.useState("offline");
+  console.log("alignment", alignment);
   const disableInternet = () => {
     postEmptyJson("/net/disable", debug);
   };
 
-  const handleInternetToggleClick = () => {
-    if (!netEnabled) {
+  const handleInternetToggleClick = (event, newAlignment) => {
+    if (alignment === "offline") {
       setInternetDialogOpen(true);
+      setAlignment(newAlignment);
     } else {
       disableInternet();
+      setAlignment(newAlignment);
     }
   };
 
+  useEffect(() => {
+    getJson("/version")
+      .then((res) => res.json)
+      .then((data) => setNameProduct(data))
+      .catch((err) => console.error("Error :", err));
+  }, []);
+
+  console.log("setNameProduct", nameProduct);
   return (
     <Box>
-      <Chip
-        icon={
-          netEnabled ? (
-            <AirplanemodeInactiveOutlinedIcon />
-          ) : (
-            <AirplanemodeActiveOutlinedIcon />
-          )
-        }
-        label={doI18n("components:header:offline_mode", i18n)}
-        onClick={handleInternetToggleClick}
-        color={netEnabled ? "appbar-chip-inactive" : "secondary"}
-        variant="Filled"
-      />
+      <ToggleButtonGroup
+        onChange={handleInternetToggleClick}
+        exclusive
+        color="secondary"
+        size="small"
+        value={alignment}
+        sx={{ backgroundColor: "secondary", color: "white" }}
+      >
+        <ToggleButton value="offline">
+          {doI18n("components:header:offline_mode", i18nRef.current)}
+        </ToggleButton>
+        <ToggleButton value="online">
+          {doI18n("components:header:online_mode", i18nRef.current)}
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <IconButton>
+        <Tooltip
+          title={doI18n(
+            "components:header:tooltip_offline_mode",
+            i18nRef.current,
+          )}
+        >
+          <HelpOutlineOutlinedIcon />
+        </Tooltip>
+      </IconButton>
       <InternetWarningDialog
         internetDialogOpen={internetDialogOpen}
         setInternetDialogOpen={setInternetDialogOpen}
