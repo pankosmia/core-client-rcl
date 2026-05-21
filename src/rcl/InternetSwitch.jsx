e78046dd2ce10e93b7aae26189eaef46e115f16b
lmeline <loise13@hotmail.fr>
2026-05-21 16:50:35 +0200

    changement couleur button

--- src/rcl/InternetSwitch.jsx
+++ src/rcl/InternetSwitch.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Box, IconButton, Tooltip } from "@mui/material";
+import { Box, Grid2, IconButton, Tooltip } from "@mui/material";
 import { doI18n, getJson, postEmptyJson } from "pithekos-lib";
 import InternetWarningDialog from "./InternetWarningDialog";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import I18nContext from "./contexts/i18nContext";
+import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
 export default function InternetSwitch({ netEnabled, debug = false }) {
   const { i18nRef } = useContext(I18nContext);
 
@@ -38,31 +39,46 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
   console.log("setNameProduct", nameProduct);
   return (
     <Box>
-      <ToggleButtonGroup
-        onChange={handleInternetToggleClick}
-        exclusive
-        color="secondary"
-        size="small"
-        value={alignment}
-        sx={{ backgroundColor: "secondary", color: "white" }}
-      >
-        <ToggleButton value="offline">
-          {doI18n("components:header:offline_mode", i18nRef.current)}
-        </ToggleButton>
-        <ToggleButton value="online">
-          {doI18n("components:header:online_mode", i18nRef.current)}
-        </ToggleButton>
-      </ToggleButtonGroup>
-      <IconButton>
-        <Tooltip
-          title={doI18n(
-            "components:header:tooltip_offline_mode",
-            i18nRef.current,
-          )}
-        >
-          <HelpOutlineOutlinedIcon />
-        </Tooltip>
-      </IconButton>
+      <Grid2 container alignItems="center" spacing={1}>
+        <Grid2>
+          <ToggleButtonGroup
+            onChange={handleInternetToggleClick}
+            exclusive
+            color="secondary"
+            size="small"
+            value={alignment}
+          >
+            <ToggleButton
+              value="offline"
+              sx={{ color: "white", "&.Mui-selected": { color: "white" } }}
+            >
+              <DoneOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+              {doI18n("components:header:offline_mode", i18nRef.current)}
+            </ToggleButton>
+
+            <ToggleButton
+              value="online"
+              sx={{ color: "white", "&.Mui-selected": { color: "white" } }}
+            >
+              {doI18n("components:header:online_mode", i18nRef.current)}
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Grid2>
+
+        <Grid2>
+          <Tooltip
+            title={doI18n(
+              "components:header:tooltip_offline_mode",
+              i18nRef.current,
+            )}
+          >
+            <IconButton sx={{ color: "white" }}>
+              <HelpOutlineOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </Grid2>
+      </Grid2>
+
       <InternetWarningDialog
         internetDialogOpen={internetDialogOpen}
         setInternetDialogOpen={setInternetDialogOpen}
