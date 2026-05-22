df3101e2d9ed674a725cbec5f9c28ecbbffaae12
lmeline <loise13@hotmail.fr>
2026-05-22 11:36:40 +0200

    new version button offline

--- src/rcl/InternetSwitch.jsx
+++ src/rcl/InternetSwitch.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Box, Grid2, IconButton, Tooltip } from "@mui/material";
+import { Box, Grid2 } from "@mui/material";
 import { doI18n, getJson, postEmptyJson } from "pithekos-lib";
 import InternetWarningDialog from "./InternetWarningDialog";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import I18nContext from "./contexts/i18nContext";
 import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
+import { alpha } from "@mui/material/styles";
+import IconInfo from "./App/IconInfo";
+
 export default function InternetSwitch({ netEnabled, debug = false }) {
   const { i18nRef } = useContext(I18nContext);
 
@@ -31,7 +33,7 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
   useEffect(() => {
     getJson("/version")
       .then((res) => res.json)
-      .then((data) => setNameProduct(data))
+      .then((data) => setNameProduct(data.product_name))
       .catch((err) => console.error("Error :", err));
   }, []);
 
@@ -46,7 +48,9 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
             color="secondary"
             size="small"
             value={alignment}
-            sx={{ background: "rgba(255, 255, 255,0.3)" }}
+            sx={{
+              background: (theme) => alpha(theme.palette.common.white, 0.3),
+            }}
           >
             <ToggleButton
               value="offline"
@@ -54,12 +58,13 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
                 color: "white",
                 "&.Mui-selected": {
                   color: "white",
-                  backgroundColor: "rgba(156, 39, 176, 0.5)",
+                  backgroundColor: (theme) =>
+                    alpha(theme.palette.secondary.main, 1),
                 },
               }}
             >
               {alignment === "offline" && (
-                <DoneOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+                <DoneOutlinedIcon fontSize="small" sx={{ paddingRight: 1 }} />
               )}
               {doI18n("components:header:offline_mode", i18nRef.current)}
             </ToggleButton>
@@ -70,12 +75,13 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
                 color: "white",
                 "&.Mui-selected": {
                   color: "white",
-                  backgroundColor: "rgba(156, 39, 176, 0.5)",
+                  backgroundColor: (theme) =>
+                    alpha(theme.palette.secondary.main, 1),
                 },
               }}
             >
               {alignment === "online" && (
-                <DoneOutlinedIcon fontSize="small" sx={{ mr: 1 }} />
+                <DoneOutlinedIcon fontSize="small" sx={{ paddingRight: 1 }} />
               )}
               {doI18n("components:header:online_mode", i18nRef.current)}
             </ToggleButton>
@@ -83,16 +89,12 @@ export default function InternetSwitch({ netEnabled, debug = false }) {
         </Grid2>
 
         <Grid2>
-          <Tooltip
-            title={doI18n(
+          <IconInfo
+            tooltipLabel={doI18n(
               "components:header:tooltip_offline_mode",
               i18nRef.current,
-            )}
-          >
-            <IconButton sx={{ color: "white" }}>
-              <HelpOutlineOutlinedIcon />
-            </IconButton>
-          </Tooltip>
+            ).replace("{1}", nameProduct)}
+          />
         </Grid2>
       </Grid2>
 
