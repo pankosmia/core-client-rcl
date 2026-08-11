e8a88dc606172b595d184a3998a243e83d5d2057
Mark Howe <mvahowe@gmail.com>
2026-08-11 14:15:35 +0200

    Fix top margin in ScrollableBody

--- src/rcl/ScrollableBody.jsx
+++ src/rcl/ScrollableBody.jsx
@@ -6,7 +6,7 @@ export default function ScrollableBody({ isAndroid, children }) {
       sx={{
         mb: 2,
         position: "fixed",
-        top: isAndroid ? "94px" : "64px",
+        top: isAndroid ? "78px" : "48px",
         left: isAndroid ? "30px" : 0,
         bottom: isAndroid ? "30px" : 0,
         right: isAndroid ? "30px" : 0,
